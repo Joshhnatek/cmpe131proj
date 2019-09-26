67abeccc34056d17aac4aa2ee139d05eb9bc5282
Emil Agonoy <emilagonoy@gmail.com>
2019-09-25 17:51:15 -0700

    Set up Home Page

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return ("Broke 'n Hungry")
+    return render_template("home.html")
 
+@app.route("/ingredients")
+def ingredients():
+    return ("Ingredients List will be here")
+     
 if __name__ == "__main__":
     app.run()
