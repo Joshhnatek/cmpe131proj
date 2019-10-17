de7717971ad85f84bd291390f7395e0866435c39
Emil Agonoy <emil.agonoy2@gmail.com>
2019-10-17 16:50:42 -0700

    Still Working on setting up a database

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    
+    SQALCHEMY_DATABASE_URI = 'sqlite:////~/CMPE131/Project/cmpe131proj/app/databases'
     SQALCHEMY_TRACK_MODIFICATIONS = False
