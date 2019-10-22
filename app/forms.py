7a80726d283f31b5bf57735358f6a70e88d44e22
Emil Agonoy <emil.agonoy2@gmail.com>
2019-10-21 17:46:33 -0700

    started registration form

--- app/forms.py
+++ app/forms.py
@@ -6,3 +6,21 @@ class User_Form(FlaskForm):
     password = PasswordField('Password')
     remember = BooleanField('Keep Me Logged In')
     submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm): 
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequried(), EqualTo('Password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+    
--- app/models.py
+++ app/models.py
@@ -1,7 +1,6 @@
 from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
 
-
-#todo password_hash
 class User(db.Model):
     id =  db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -11,6 +10,12 @@ class User(db.Model):
 
     def __repr(self):
         return '{}'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 @login.user_loader
 def load_user(id):
