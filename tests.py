ec020ad39025feacddc9790d5ae7e102214f5a2e
Emil Agonoy <emil.agonoy2@gmail.com>
2019-11-03 18:38:37 -0800

    Started tests.py script

--- tests.py
+++ tests.py
@@ -2,13 +2,14 @@ from app import app, db
 from app.models import User 
 
 def test_No_User():
-    u = User.query.filter_by(username = "Not_Existing_User"); 
-    assert (u is None)
-
-
-
+    print("Search User Database for 'Not_Existing_User'")
+    u = User.query.filter_by(username = "Not_Existing_User").first(); 
+    
+    assert (u == None)
 
 if __name__ == "__main__":
     print("Starting Tests")
 
     test_No_User()
+
+    print("Tests Finished")
