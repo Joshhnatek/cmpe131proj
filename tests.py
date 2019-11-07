1e39ca7ad94861de989cc356c1f081d5d2076ca5
Emil Agonoy <emil.agonoy2@gmail.com>
2019-11-06 16:53:50 -0800

    Added existing user test

--- tests.py
+++ tests.py
@@ -2,14 +2,21 @@ from app import app, db
 from app.models import User 
 
 def test_No_User():
-    print("Search User Database for 'Not_Existing_User'")
+    print("Non-Existing User Test")
     u = User.query.filter_by(username = "Not_Existing_User").first(); 
     
     assert (u == None)
 
+def test_Existing_User():
+    print("Existing User Test"); 
+    u = User.query.filter_by(email = "test@email.com").first(); 
+
+    assert (u.username == "Test_Dude1"); 
+
 if __name__ == "__main__":
-    print("Starting Tests")
+    print("\n___Starting Tests___\n")
 
     test_No_User()
+    test_Existing_User(); 
 
-    print("Tests Finished")
+    print("\n___Tests Finished___\n")
