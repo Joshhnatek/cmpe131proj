6c890f4aa5220bb5673de52270a7b4a7b396bdfb
Salleek <sbstevencai@gmail.com>
2019-11-24 23:55:07 -0800

    Fixed issue with app.db not opening

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from app.config import Config
 from app.ingredients_list import meats, spices, carbohydrates, vegetables, fruits
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-#from flask_migrate import Migrate
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -11,13 +11,13 @@ app.config.from_object(Config)
 app.config['SECRET_KEY'] = Config.SECRET_KEY
 
 db = SQLAlchemy(app)
-#migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
 @app.before_first_request
 def create_tables():
-    from app.models import User, Ingredients
+    from app.models import User, Ingredients, Pantry
     db.create_all()
 
 from app import routes, models, forms
--- app/config.py
+++ app/config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = 'something something'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///databases/app.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
